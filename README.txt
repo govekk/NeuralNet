cb2a66424b7042e5844cb18aff539c88db5a3302
Kiya Govek <kiyagovek@eduroam-173-27.dyn.carleton.edu>
2017-03-11 11:48:46 -0600

    better python conventions

---
+++ README.txt
@@ -0,0 +1,9 @@
+CS321 Artificial Intelligence final project - Ryan Gorey and Kiya Govek
+
+Neural network to perform regression on wine data uses:
+- one hidden layer
+- sigmoid neurons
+- stochastic gradient descent
+- 70% training data, 15% development data, 15% test data
+
+TO RUN:
--- import_data.py
+++ import_data.py
@@ -2,35 +2,50 @@ import csv
 
 # data object takes in a list of info about wine
 # stores info and creates getter methods for the features
-class wine_data:
+
+
+class WineData:
     def __init__(self, wine_info):
         self.features = wine_info[:-1]
         self.quality = wine_info[-1]
+
     def get_fixed_acidity(self):
         return self.features[0]
+
     def get_volatile_acidity(self):
         return self.features[1]
+
     def get_citric_acid(self):
         return self.features[2]
+
     def get_residual_sugar(self):
         return self.features[3]
+
     def get_chlorides(self):
         return self.features[4]
+
     def get_free_SO2(self):
         return self.features[5]
+
     def get_total_SO2(self):
         return self.features[6]
+
     def get_density(self):
         return self.features[7]
+
     def get_pH(self):
         return self.features[8]
+
     def get_sulphates(self):
         return self.features[9]
+
     def get_alcohol(self):
         return self.features[10]
+
     # this is our output variable
     def get_quality(self):
         return self.quality
+
 
 # import data from one file and create a list of wine_data objects for it
 def import_data_file(file_name):
@@ -44,14 +59,15 @@ def import_data_file(file_name):
             first_row = False
         # for the rest of the rows, create a wine_data object and append to list
         else:
-            data_list.append(wine_data(row))
+            data_list.append(WineData(row))
     return data_list
+
 
 # import data for all training, dev, and test files for use later
 def import_data():
     training_data = import_data_file("winequality-white.csv")
-    #dev_data = import_data_file("dev_data.csv")
-    #test_data = import_data_file("test_data.csv")
+    # dev_data = import_data_file("dev_data.csv")
+    # test_data = import_data_file("test_data.csv")
     print training_data[0].get_chlorides()
 
 if __name__ == "__main__":
