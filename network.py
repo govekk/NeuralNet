47b2dfcf7c79847cb72905f10a0e0cc6a9b01bfd
Ryan Gorey <goreyr@carleton.edu>
2017-03-13 14:25:13 -0500

    Progress on implementation of backpropagation algorithm

--- network.py
+++ network.py
@@ -12,8 +12,10 @@ the quality of new wines.
 ### PACKAGES ###
 
 import numpy as np
+import random
 
 ### HELPER FUNCTIONS ###
+
 """
 Generates a random real number between -1 and 1.
 
@@ -22,7 +24,7 @@ Parameters: None
 Returns a real number between -1 and 1.
 """
 def randInitialVal():
-    return 0
+    return random.uniform(-1, 1)
 
 ### NEURAL NETWORK ###
 
@@ -48,8 +50,6 @@ class NeuralNetwork:
         w1 = np.array([[randInitialVal() for k in range(11)] for j in range(num_hidden)])
         w2 = np.array([[randInitialVal() for k in range(num_hidden)] for j in range(1)])
         self.weights = [w1, w2]
-        print(w1)
-        print(w2)
 
         # Initialize bias matrices
         b1 = np.array([randInitialVal() for j in range(num_hidden)])
@@ -58,20 +58,10 @@ class NeuralNetwork:
 
         self.output_bias = 0
         self.learning_rate = learning_rate
-        self.num_hidden = num_hidden
 
-    """
-    Initializes weights as random values between 0 and 1
+        assert(int(num_hidden) > 0)
+        self.num_hidden = int(num_hidden)
 
-    Parameters: None.
-
-    Returns nothing.
-    """
-    def init_weights(self):
-        pass
-
-    # input: weighted input (z) (vector)
-    # output: activation (a) (vector)
     """
     Applies the sigmoid function to vector of weighted inputs z.
 
@@ -79,27 +69,95 @@ class NeuralNetwork:
 
     Returns vector of activations.
     """
-    def vectorizedSigmoid(self, z):
-        # iterate in here
-        return
+    def vectorizedSigmoid(self, z_vector):
+        for i in range(len(z_vector)):
+            z_vector[i] = sigmoid(z_vector[i])
+
+        return z_vector
 
     """
-    Trains the neural netword on training data, preparing it for new
+    Applies the sigmoid function to a single weighted input z.
+
+    Parameters:
+    :z: - the weighted input for a sigmoid neuron
+
+    Returns the activation (float) using sigmoid function:
+
+            1
+        _________
+
+        1 + e^(-z)
+    """
+    def sigmoid(self, z):
+        return (1/(1 + exp(-z)))
+
+    """
+    Trains the neural network on training data, preparing it for new
     predictions. Uses stochastic gradient descent and backpropagation.
 
     Parameters:
-    :num_iter: - the number of times to iterate through training data (int)
-    :training_data: - the data used to train the network
+    :num_epochs: - the number of times to iterate through training data (int)
+    :training_data: - the data used to train the network. Comes in a list of
+    lists, where each row is a data point and each column is a feature.
 
     Returns nothing.
     """
-    def train(self, num_iter, training_data):
-        for i in range(num_iter):
+    def train(self, num_epochs, training_data):
+        for i in range(num_epochs):
             for training_point in training_data:
                 z_hidden, z_output = self.feed_forward(training_point)
                 error_output = self.calculate_output_error(z_output, training_point[-1])
                 error_hidden = self.backpropagate(error_output, z_hidden)
             self.gradient_descent()
+
+
+    """
+    Iterates through the neural network, calculating the weighted inputs z
+    along the way (for a specific training data point).
+
+    Parameters:
+    :training_point: - a single case of training data
+
+    Returns the weighted inputs z in vectors by layer.
+    """
+    def feed_forward(self, training_point):
+        # last column is answer
+        # activation of first row are feature values from data_point (vector)
+        # calculate z_hidden from data_point, z (vector) = wa (matrix)*(vector) + b (vector)
+        # calculate a_hidden from z_hidden in sigmoid function, 1/(1+exp(z))
+        #                                    (vector, calculated individually)
+        # calculate z_output from a_hidden, z (scaler) = wa (vector)*(vector) + b (scaler)
+
+        
+
+
+
+
+
+
+        z_hidden, a_hidden, z_output = 1
+        return z_hidden, z_output
+
+    # input: weighted input z of output neuron (vector length 1), correct output
+    # output: error of output layer (vector)
+    def calculate_output_error(self, z_output, answer):
+        # output derivative = 10 if multiply by 10
+        a_output = self.sigmoid(z_output)
+        # return (a_output - answer) * 10 < make this a vector!!!
+
+    # backpropagates errors from output to hidden layer
+    # input: error of output layer (vector), weighted input of hidden layer (vector)
+    # output: error of hidden layer (vector)
+    def backpropagate(self, error_output, z_hidden):
+        # (self.weights[1] (transposed?) *error_output) * (sig'(z) = (sig(z) (1 - sig(z)))
+        pass
+
+    # performs gradient descent to update weights for one iteration of training
+    # input: sum of errors, sum of errors*activations
+    # output: none
+    # side effects: changes weight and bias instance variables
+    def gradient_descent(self, ):
+        pass
 
     """
     Updates the weights in accordance with gradient descent rule.
@@ -129,47 +187,9 @@ class NeuralNetwork:
     # output: weighted input z of hidden layer (vector),
     #           weighted input z of output layer (vector)
 
-    """
-    Iterates through the neural network, calculating the weighted inputs
-    along the way (for a specific training data point).
-
-    Parameters:
-    :training_point: - a single case of training data
-
-    Returns the weighted inputs for the neurons.
-    """
-    def feed_forward(self, training_point):
-        # last column is answer
-        # activation of first row are feature values from data_point (vector)
-        # calculate z_hidden from data_point, z (vector) = wa (matrix)*(vector) + b (vector)
-        # calculate a_hidden from z_hidden in sigmoid function, 1/(1+exp(z))
-        #                                    (vector, calculated individually)
-        # calculate z_output from a_hidden, z (scaler) = wa (vector)*(vector) + b (scaler)
-        z_hidden, a_hidden, z_output = 1
-        return z_hidden, z_output
-
-    # input: weighted input z of output neuron (vector length 1), correct output
-    # output: error of output layer (vector)
-    def calculate_output_error(self, z_output, answer):
-        # output derivative = 10 if multiply by 10
-        a_output = self.sigmoid(z_output)
-        # return (a_output - answer) * 10 < make this a vector!!!
-
-    # backpropagates errors from output to hidden layer
-    # input: error of output layer (vector), weighted input of hidden layer (vector)
-    # output: error of hidden layer (vector)
-    def backpropagate(self, error_output, z_hidden):
-        # (self.weights[1] (transposed?) *error_output) * (sig'(z) = (sig(z) (1 - sig(z)))
-        pass
-
-    # performs gradient descent to update weights for one iteration of training
-    # input: sum of errors, sum of errors*activations
-    # output: none
-    # side effects: changes weight and bias instance variables
-    def gradient_descent(self, ):
-        pass
 
 def main():
     myNet = NeuralNetwork()
+    myNet.train()
 
 main()
