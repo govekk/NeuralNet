68924ef9192c581c2cc5393056237d6f13e60482
goreyr <goreyr@carleton.edu>
2017-03-15 14:37:58 -0500

    fixed typo

--- wine_predictor.py
+++ wine_predictor.py
@@ -32,7 +32,7 @@ def getRMSE(preds):
     return RMSE
 
 def main():
-    wine_net = network.NeuralNetwork(learning_rates=0.1, hidden_neurons=10)
+    wine_net = network.NeuralNetwork(learning_rate=0.1, hidden_neurons=10)
     wine_net.train(training_data, batch_size=256, num_epochs=1000)
     preds = wine_net.get_predictions(dev_data)
     print(getRMSE(preds))
